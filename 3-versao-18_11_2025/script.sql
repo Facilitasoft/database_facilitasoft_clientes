484c42d872042c433fd59e31ae33934640f86458
DANIEL VIANA <facilitasoft.cloud@gmail.com>
2025-11-22 14:00:25 -0300

    update

--- 3-versao-18_11_2025/script.sql
+++ 3-versao-18_11_2025/script.sql
@@ -1,2 +1,8 @@
 ALTER TABLE clientes.assinatura
 RENAME COLUMN id_aluno TO id_cliente;
+
+alter table permissao 
+drop column nome_role;
+
+alter table permissao 
+add column nome_role text not null;

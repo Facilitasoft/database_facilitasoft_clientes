c0ab1c0d5f703a744f948a0281ef09a54e06625b
DANIEL VIANA <facilitasoft.cloud@gmail.com>
2025-11-22 14:14:55 -0300

    update

--- 3-versao-18_11_2025/script.sql
+++ 3-versao-18_11_2025/script.sql
@@ -9,3 +9,10 @@ add column nome_role text not null;
 
 alter table permissao
 add column client_id_keycloak text not null
+
+INSERT INTO clientes.permissao (id_permissao, id_plano, nome_role, client_id_keycloak)
+VALUES
+	('1', 'plano_academia_free',   'gestao_academia:free',   'web--gestao-academia'),
+	('2', 'plano_academia_basico', 'gestao_academia:basico', 'web--gestao-academia'),
+	('3', 'plano_academia_prata',  'gestao_academia:prata',  'web--gestao-academia'),
+	('4', 'plano_academia_ouro',   'gestao_academia:ouro',   'web--gestao-academia');
